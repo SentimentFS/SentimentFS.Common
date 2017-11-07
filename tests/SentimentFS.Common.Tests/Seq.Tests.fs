defad3cc7f23249d3f37fa73549d86bd438dcbfe
Dominik Kotecki <dominikus1910@gmail.com>
2017-11-07 18:07:00 +0100

    small fix

--- tests/SentimentFS.Common.Tests/Seq.Tests.fs
+++ tests/SentimentFS.Common.Tests/Seq.Tests.fs
@@ -7,13 +7,13 @@ module Seq =
 
     [<Tests>]
     let tests =
-        testList "List" [
+        testList "Seq" [
             testList "filterOut" [
                 testCase "delete when elements exist in  filter list" <| fun _ ->
                     let subject = ["A"; "B"; "C"] |> Seq.filterOut ["C"]
-                    Expect.equal subject (["A";"B"] |> List.toSeq) "should equal ['A';'B']"
+                    Expect.sequenceEqual subject (["A";"B"] |> List.toSeq) "should equal ['A';'B']"
                 testCase "delete when elements no exist in filter list" <| fun _ ->
                     let subject = ["A"; "B"; "C"] |> Seq.filterOut ["D"]
-                    Expect.equal subject (["A";"B";"C"] |> List.toSeq) "should equal ['A';'B';'C']"
+                    Expect.sequenceEqual subject (["A";"B";"C"] |> List.toSeq) "should equal ['A';'B';'C']"
 ]
         ]
